787c6b52578d6410939daa96c4ce20cab931e660
AJ1732 <ejemeniboi@gmail.com>
2024-06-05 20:49:45 +0100

    refactor: comment out blob

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     <main className='relative space-y-2'>
       <section className=' h-96 max-w-80 m-auto flex justify-center items-center'>
         {/* BLOB */}
-        <span 
-          className='blob -z-20 size-40 absolute left-0 -top-32 rounded-full blur-3xl'
-        ></span>
+        {/* <span 
+          className='blob -z-20 size-40 absolute -left-10 -top-40 rounded-full blur-3xl'
+        ></span> */}
         
         <h2 className='text-2xl text-center font-thin text-primary-100'>Compilation of Framer Motion styles</h2>
       </section>
