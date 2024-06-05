9b2663840d68648209afef8490bd5eb6304c135c
AJ1732 <ejemeniboi@gmail.com>
2024-06-05 20:24:45 +0100

    refactor: rename folder Scroll to scroll

---
+++ src/pages/scroll/HorizontalScroll.jsx
@@ -0,0 +1,69 @@
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+const cards = [1, 2, 3, 4, 5, 6, 7]
+
+const HorizontalScroll = () => {
+  return (
+    <div className="bg-neutral-800">
+      <div className="flex h-48 items-center justify-center">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll down
+        </span>
+      </div>
+
+      <HorizontalScrollCarousel />
+
+      <div className="flex h-48 items-center justify-center">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll up
+        </span>
+      </div>
+    </div>
+  );
+};
+
+const HorizontalScrollCarousel = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div style={{ x }} className="flex gap-4">
+          {cards.map((card) => {
+            return <Card card={card} key={card} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+const Card = ({ card }) => {
+  return (
+    <div
+      key={card}
+      className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
+    >
+      <div
+        style={{
+          // backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute size-32 inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      ></div>
+      <div className="absolute inset-0 z-10 grid place-content-center">
+        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+          Card {card}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HorizontalScroll;
---
+++ src/pages/scroll/Scroll.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import Transition from '../../components/Transition'
+
+const Scroll = () => {
+  return (
+    <main className='space-y-4'>
+      <nav>
+        <ul className='flex justify-start items-center divide-x divide-shade-800 border-b border-shade-800 pb-4 [&>*]:px-5'>
+          <Link to={`/scroll/horizontal`} className='hover:bg-zinc-700 active:bg-primary-500 rounded-lg py-2 transition-all duration-300 ease-in-out'>
+            <li>Horizontal Scroll</li>
+          </Link>
+          <Link to={`/scroll/page`} className='hover:bg-zinc-700 active:bg-primary-500 rounded-lg py-2 transition-all duration-300 ease-in-out'>
+            <li>Page Scroll</li>
+          </Link>
+
+        </ul>
+      </nav>
+
+      <section>
+        <Outlet />
+      </section>
+    </main>
+  )
+}
+
+export default Transition(Scroll)
