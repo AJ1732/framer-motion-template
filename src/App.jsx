7e02fc8df6c5c5e43f5dd5db11aecc97a77dcdf9
AJ1732 <ejemeniboi@gmail.com>
2024-06-19 22:32:02 +0100

    chore: comment out blob

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <div className="relative py-4">
           <Outlet />
           {/* BLOB */}
-          <span className="absolute -right-10 bottom-1/2 -z-20 h-12 w-20 rounded-full bg-primary-500 blur-[20rem]"></span>
+          {/* <span className="absolute -right-10 bottom-1/2 -z-20 h-12 w-20 rounded-full bg-primary-500 blur-[20rem]"></span> */}
         </div>
       </div>
     </AnimatePresence>
