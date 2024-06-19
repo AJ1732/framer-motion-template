aa07fa44597ebaf1252ee60a655db9ca2b72cb5b
AJ1732 <ejemeniboi@gmail.com>
2024-06-19 23:03:00 +0100

    chore: creat flip animation

--- src/components/ui/Flip.jsx
+++ src/components/ui/Flip.jsx
@@ -1,4 +1,56 @@
+import { motion } from "framer-motion";
+
+const DURATION = 0.25;
+const STAGGER = 0.025;
+
 const Flip = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="hovered"
+      className="relative block overflow-hidden whitespace-nowrap text-4xl font-black uppercase tracking-tighter sm:text-7xl md:text-8xl lg:text-9xl"
+      style={{ lineHeight: 0.75 }}
+    >
+      <div>
+        {children.split("").map((l, i) => (
+          <motion.span
+            key={i}
+            className="inline-block"
+            variants={{
+              initial: { y: 0 },
+              hovered: { y: "-100%" },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => (
+          <motion.span
+            key={i}
+            className="inline-block"
+            variants={{
+              initial: { y: "100%" },
+              hovered: { y: 0 },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.div>
+  );
 };
 export default Flip;
