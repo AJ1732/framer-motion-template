d4c4bfbb550a1716c32942e0e7b2d3b932f0edf6
AJ1732 <ejemeniboi@gmail.com>
2024-06-21 22:12:31 +0100

    chore: create enlarg scroll tab

--- src/pages/scroll/Scroll.jsx
+++ src/pages/scroll/Scroll.jsx
@@ -2,23 +2,37 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import Transition from "../../components/ui/Transition";
 
+const links = [
+  {
+    id: 1,
+    link: "/scroll/horizontal",
+    text: "Horizontal Scroll"
+  },
+  {
+    id: 2,
+    link: "/scroll/page",
+    text: "Page Scroll"
+  },
+  {
+    id: 3,
+    link: "/scroll/enlarge",
+    text: "Enlarge Scroll"
+  },
+]
 const Scroll = () => {
   return (
     <main className="space-y-4">
       <nav>
         <ul className="flex items-center justify-start divide-x divide-shade-800 border-b border-shade-800 pb-4 [&>*]:px-5">
-          <Link
-            to={`/scroll/horizontal`}
-            className="rounded-lg py-2 transition-all duration-300 ease-in-out hover:bg-zinc-700 active:bg-primary-500"
-          >
-            <li>Horizontal Scroll</li>
-          </Link>
-          <Link
-            to={`/scroll/page`}
-            className="rounded-lg py-2 transition-all duration-300 ease-in-out hover:bg-zinc-700 active:bg-primary-500"
-          >
-            <li>Page Scroll</li>
-          </Link>
+          {links.map(({id, link, text}) => (
+            <Link
+              key={id}
+              to={link}
+              className="rounded-lg py-2 transition-all duration-300 ease-in-out hover:bg-zinc-700 active:bg-primary-500"
+            >
+              <li>{text}</li>
+            </Link>
+          ))}
         </ul>
       </nav>
 
