da2621e74fc8566eb034856680c10e9744bc143b
AJ1732 <ejemeniboi@gmail.com>
2024-06-22 18:59:10 +0100

    chore: build bubble text animation component

--- src/components/ui/Bubble.jsx
+++ src/components/ui/Bubble.jsx
@@ -1,4 +1,16 @@
-const Bubble = () => {
-  return <div>Bubble</div>;
+const Bubble = ({ children }) => {
+  return (
+    <span className="font-poppins text-center text-5xl font-thin text-primary-300">
+      {children.split("").map((child, idx) => (
+        <span
+          key={idx}
+          // Using tailwindcss-selector-patterns plugin
+          className="pattern-[+]:hover:font-medium pattern-[+]:hover:text-primary-200 pattern-[+2]:hover:font-light pattern-[-]:hover:font-medium pattern-[-2]:hover:font-light transition-all duration-500 hover:font-black hover:text-primary-100"
+        >
+          {child}
+        </span>
+      ))}
+    </span>
+  );
 };
 export default Bubble;
--- src/pages/BubbleText.jsx
+++ src/pages/BubbleText.jsx
@@ -1,4 +1,10 @@
+import Bubble from "../components/ui/Bubble";
+
 const BubbleText = () => {
-  return <div>BubbleText</div>;
+  return (
+    <section className="min-h-80 flex justify-center items-center">
+      <Bubble>BubbbbbbbbbleText</Bubble>
+    </section>
+  );
 };
 export default BubbleText;
