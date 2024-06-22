b7743b923816dec8c21e1b52de8d5d7f38ac2391
AJ1732 <ejemeniboi@gmail.com>
2024-06-22 18:21:55 +0100

    chore: import poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       colors: {
         primary: {
           50: "#FFFCED",
