dfa2108b444ed9d2a72d293fcb8799b97e42e502
AJ1732 <ejemeniboi@gmail.com>
2024-06-22 18:03:31 +0100

    chore: build enlarge div on scroll animation

---
+++ src/components/ui/Enlarge.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const Enlarge = ({ item }) => {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scaleProgress,
+        opacity: scrollYProgress,
+      }}
+      className="min-h-80 w-full rounded bg-zinc-500 px-4 py-3"
+    >
+      <section className="min-h-80 w-full rounded bg-zinc-400 px-4 py-3">
+        EnlargeScroll {item}
+      </section>
+    </motion.div>
+  );
+};
+export default Enlarge;
--- src/pages/scroll/EnlargeScroll.jsx
+++ src/pages/scroll/EnlargeScroll.jsx
@@ -1,4 +1,23 @@
+import { Fragment } from "react";
+import Enlarge from "../../components/ui/Enlarge";
+
 const EnlargeScroll = () => {
-  return <div>EnlargeScroll</div>;
+  return (
+    <section className="flex flex-col gap-4 pb-40">
+      <header className="flex min-h-[420px] items-center justify-center">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll
+        </span>
+      </header>
+
+      <div className={`space-y-4`}>
+        {[1, 2, 3, 4].map((item) => (
+          <Fragment key={item}>
+            <Enlarge item={item}></Enlarge>
+          </Fragment>
+        ))}
+      </div>
+    </section>
+  );
 };
 export default EnlargeScroll;
