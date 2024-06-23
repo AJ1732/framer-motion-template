dc46f43f4745c59a55b0f10426257595b734b296
AJ1732 <ejemeniboi@gmail.com>
2024-06-23 22:34:16 +0100

    chore: import shiny button

--- src/pages/Buttons.jsx
+++ src/pages/Buttons.jsx
@@ -1,12 +1,14 @@
+import Transition from "../components/ui/Transition";
 import SpotLightButton from "../components/ui/buttons/SpotLightButton";
 import AuroraButton from "../components/ui/buttons/AuroraButton";
-import Transition from "../components/ui/Transition";
+import ShinyButton from "../components/ui/buttons/ShinyButton";
 
 const Buttons = () => {
   return (
     <div className="flex min-h-80 flex-col items-center justify-start gap-10 py-4">
       <SpotLightButton />
-      <AuroraButton> Aura Button</AuroraButton>
+      <AuroraButton>Aura Button</AuroraButton>
+      <ShinyButton>Shiny Button</ShinyButton>
     </div>
   );
 };
