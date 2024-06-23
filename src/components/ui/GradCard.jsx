58e6c92f4fd0a4e83491348f689cc8968d2494ad
AJ1732 <ejemeniboi@gmail.com>
2024-06-23 21:29:31 +0100

    chore: adjust inset

--- src/components/ui/GradCard.jsx
+++ src/components/ui/GradCard.jsx
@@ -1,6 +1,6 @@
 const GradCard = ({ children }) => {
   return (
-    <div className="before:bg-gradient before:animate-rotation after:animate-rotation after:bg-gradient relative aspect-[1/1.5] h-full rounded-lg bg-zinc-900 px-4 py-3 font-bold text-primary-100 before:absolute before:-inset-0.5 before:-z-10 before:rounded-lg after:absolute after:-inset-0.5 after:-z-10 after:rounded-lg after:blur-[3.5rem]">
+    <div className="before:bg-gradient before:animate-rotation after:animate-rotation after:bg-gradient relative aspect-[1/1.5] h-full rounded-lg bg-zinc-900 px-4 py-3 font-bold text-primary-100 before:absolute before:-inset-[1px] before:-z-10 before:rounded-lg after:absolute after:-inset-[1px] after:-z-10 after:rounded-lg after:blur-[3.5rem]">
       {children}
     </div>
   );
