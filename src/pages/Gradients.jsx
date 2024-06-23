5b1525d28fe197b78fe59abe3c818eb7c3840a58
AJ1732 <ejemeniboi@gmail.com>
2024-06-23 21:25:33 +0100

    chore: import gradient card component

--- src/pages/Gradients.jsx
+++ src/pages/Gradients.jsx
@@ -1,18 +1,32 @@
 import AuroraGrad from "../components/ui/AuroraGrad";
+import GradCard from "../components/ui/GradCard";
 
 const Gradients = () => {
   return (
-    <section>
+    <section className="space-y-36">
       {/* RADIAL GRADIENT */}
-      <article>
+      <article className="space-y-2">
         <header>
           <span className="font-semibold uppercase text-neutral-500">
             Radial Gradients
           </span>
         </header>
 
-        <div className="h-96 flex justify-center items-center">
+        <div className="flex h-96 items-center justify-center">
           <AuroraGrad />
+        </div>
+      </article>
+
+      {/* CARD GRADIENT */}
+      <article className="space-y-8">
+        <header>
+          <span className="font-semibold uppercase text-neutral-500">
+            Card Gradient
+          </span>
+        </header>
+
+        <div className="flex h-96 items-center justify-center">
+          <GradCard />
         </div>
       </article>
     </section>
