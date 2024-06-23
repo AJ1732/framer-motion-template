9cc668eafd993d92b7f43fa38385365df938c97a
AJ1732 <ejemeniboi@gmail.com>
2024-06-23 23:29:44 +0100

    chore: build shiny button component

--- src/components/ui/buttons/ShinyButton.jsx
+++ src/components/ui/buttons/ShinyButton.jsx
@@ -1,4 +1,33 @@
-const ShinyButton = () => {
-  return <div>ShinyButton</div>;
+import { motion } from "framer-motion";
+
+const ShinyButton = ({ children }) => {
+  return (
+    <motion.button
+      initial={{ "--x": "100%" }}
+      animate={{ "--x": "-100%" }}
+      whileTap={{ scale: 0.97 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 1,
+        type: "spring",
+        stiffness: 20,
+        damping: 15,
+        mass: 2,
+        scale: {
+          type: "spring",
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
+      className="radial-gradient relative rounded-md px-6 py-2"
+    >
+      <span className="linear-mask relative block size-full font-light tracking-wide text-neutral-100">
+        {children}
+      </span>
+      <span className="linear-overlay absolute inset-0 block rounded-md p-px"></span>
+    </motion.button>
+  );
 };
 export default ShinyButton;
