90855fe8f75ddfa0ee65c5f7f42e0a3fc83799cb
AJ1732 <ejemeniboi@gmail.com>
2024-06-23 10:37:32 +0100

    chore: update spacing

--- src/pages/Buttons.jsx
+++ src/pages/Buttons.jsx
@@ -4,9 +4,9 @@ import Transition from "../components/ui/Transition";
 
 const Buttons = () => {
   return (
-    <div className="flex min-h-80 flex-col items-center justify-start py-4">
+    <div className="flex min-h-80 flex-col items-center justify-start gap-10 py-4">
       <SpotLightButton />
-      <AuroraButton>Button</AuroraButton>
+      <AuroraButton> Aura Button</AuroraButton>
     </div>
   );
 };
