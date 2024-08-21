79dc791cced836f18332de5ab137d71d292cd342
AJ1732 <ejemeniboi@gmail.com>
2024-08-21 20:55:01 +0100

    refactor: update marquee to three steps

--- src/pages/scroll/Marquee.jsx
+++ src/pages/scroll/Marquee.jsx
@@ -3,17 +3,20 @@ import { skills } from "../../data/db";
 import FlipDiv from "../../components/ui/div/FlipDiv";
 
 const Marquee = () => {
+  const oneThirdLength = Math.floor(skills.length / 3)
+  const oneHalfLength = Math.ceil(skills.length / 2)
+
   return (
     <div className="mx-auto space-y-6">
       <div className="flex overflow-hidden x-gradient">
         <motion.div
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+          transition={{ duration: 25, repeat: Infinity, ease: "linear" }}
           className="flex flex-shrink-0 *:mr-6"
         >
           {skills
-            .slice(0, Math.ceil(skills.length / 2))
+            .slice(0, oneThirdLength)
             .map(({ id, skill, icon }, i) => (
               // <div
               //   key={`un-${i}`}
@@ -29,11 +32,11 @@ const Marquee = () => {
         <motion.div
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
-          transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+          transition={{ duration: 25, repeat: Infinity, ease: "linear" }}
           className="flex flex-shrink-0 *:mr-6"
         >
           {skills
-            .slice(0, Math.ceil(skills.length / 2))
+            .slice(0, oneThirdLength)
             .map(({ id, skill, icon }, i) => (
               // <div
               //   key={`un-${i}`}
@@ -54,7 +57,7 @@ const Marquee = () => {
           className="flex flex-shrink-0 *:mr-6"
         >
           {skills
-            .slice(Math.ceil(skills.length / 2))
+            .slice(oneThirdLength, oneThirdLength * 2)
             .map(({ id, skill, icon }, i) => (
               // <div
               //   key={`un-${i}`}
@@ -74,7 +77,7 @@ const Marquee = () => {
           className="flex flex-shrink-0 *:mr-6"
         >
           {skills
-            .slice(Math.ceil(skills.length / 2))
+            .slice(oneThirdLength, oneThirdLength * 2)
             .map(({ id, skill, icon }, i) => (
               // <div
               //   key={`un-${i}`}
@@ -83,6 +86,47 @@ const Marquee = () => {
               //   Skill
               // </div>
               <FlipDiv key={id} text={skill} under={icon} />
+            ))}
+        </motion.div>
+      </div>
+
+      <div className="flex overflow-hidden x-gradient">
+        <motion.div
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+          className="flex flex-shrink-0 *:mr-6"
+        >
+          {skills
+            .slice(oneThirdLength * 2)
+            .map(({ id, skill, icon }, i) => (
+              // <div
+              //   key={`un-${i}`}
+              //   className="rounded-lg bg-neutral-300/10 px-6 py-4 text-lg mr-6"
+              // >
+              //   Skill
+              // </div>
+
+              <FlipDiv key={id} text={skill} under={icon} className={"mr-6"} />
+            ))}
+        </motion.div>
+
+        <motion.div
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+          className="flex flex-shrink-0 *:mr-6"
+        >
+          {skills
+            .slice(oneThirdLength * 2)
+            .map(({ id, skill, icon }, i) => (
+              // <div
+              //   key={`un-${i}`}
+              //   className="mr-6 rounded-lg bg-red-300/10 px-6 py-4 text-lg"
+              // >
+              //   Skill
+              // </div>
+              <FlipDiv key={id} text={skill} under={icon} className={"mr-6"} />
             ))}
         </motion.div>
       </div>
